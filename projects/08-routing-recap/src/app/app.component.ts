78ea6761f5052b8c9f917ae341af085aca4b1c6f
Daniel Salazar <dsnile.salazr@gmail.com>
2024-12-24 17:39:51 -0600

    agregar enrutamiento y componentes Home y Details; mejorar AppComponent con enlaces dinámicos

--- projects/08-routing-recap/src/app/app.component.ts
+++ projects/08-routing-recap/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+/** @format */
+
+import {Component} from "@angular/core";
+import {RouterLink, RouterOutlet} from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, RouterLink],
+	template: `
+		<h1>Welcome to {{ title }}!</h1>
+		@for(product of productTitles; track product){
+		<p>
+			<a [routerLink]="['details', $index]"  >{{ product }}</a>
+		</p>
+		}
+		<router-outlet />
+	`,
 })
 export class AppComponent {
-  title = '08-routing-recap';
+	title = "08-routing-recap";
 
-  productTitles = ['Product 1', 'Product 2', 'Product 3'];
+	productTitles = ["Product 1", "Product 2", "Product 3"];
 }
